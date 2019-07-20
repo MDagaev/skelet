3b365077acfe1cf18ef30d33ef2875c04123daa5
MDagaev <tavintavan@gmail.com>
2019-07-20 12:04:00 +0800

    убираем лишнее

--- index.php
+++ index.php
@@ -1,12 +1,8 @@
 <?php
 // начальный файл
     require_once("database.php");
-    require_once("models/func.php");
-
-
+    
     $link = db_connect();
-    //$video = video_get($link, $_GET['id']); //берем 1 видео по id
-    //$video = video_getRand($link);//берем видео случайным образом из БД
 
     include("views/video.php");
 
