cfc2d76bc755f4f7511a592069997f719da9b0c3
MDagaev <tavintavan@gmail.com>
2019-07-23 15:32:31 +0800

    исправлена реализация Ajax извлечения видео из БД каждые 23 секунды

--- scripts/rotation.js
+++ scripts/rotation.js
@@ -16,20 +16,18 @@ $("document").ready(function() {
 
   //4. сама функция getVideo()
   function getVideo() {
-    $.ajax({
+      $.ajax({
       url: "./models/getVideo.php",
       type: "POST",
       //подготовка
       success: function(ansdata) {
-        //alert(ansdata);//Даные приходят из getVideo.php
+        //Даные приходят из getVideo.php
         ansdata = jQuery.parseJSON(ansdata);
-        //alert(ansdata);//данные преобразуются в объекты
+        //alert(ansdata.video.substr(17));//данные преобразуются в объекты
         //работа со строкой 30 замена части строки данными из ansdata, добавление ;mute=1;
-        $("iframe").attr(
-          "src",
-          "http://www.youtube.com/embed/" +
-            ansdata["video"] +
-            "?rel=0&amp;autoplay=1;controls=0&amp;showinfo=0"
+           $("iframe").attr(
+          "src", "http://www.youtube.com/embed/" + ansdata.video.substr(17) +
+          "?rel=0&amp;autoplay=1;controls=0&amp;showinfo=0"
         );
         //запуск автоплей  для хрома
         //$("iframe").attr("allow", "autoplay; encrypted-media");
