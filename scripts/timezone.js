0d56371e6f7b10a36952dbdb88089f815a299699
MDagaev <tavintavan@gmail.com>
2019-07-23 10:21:16 +0800

    форматирование

--- scripts/timezone.js
+++ scripts/timezone.js
@@ -1,9 +1,8 @@
-function myTimezone()
-    {
-        //здесь код вычисления таймзоны
-        var g = new Date();
-        var utc = "UTC " + (g.getTimezoneOffset())/(-60).toString();
-        return utc;
-    }
+function myTimezone() {
+  //здесь код вычисления таймзоны
+  var g = new Date();
+  var utc = "UTC " + g.getTimezoneOffset() / (-60).toString();
+  return utc;
+}
 
 document.getElementById("tzone").value = myTimezone();
